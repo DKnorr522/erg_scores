23e870c84542e171431c3cc7dcc0fa5ddb6a23ae
Dan Knorr <Daniel.Knorr@gmail.com>
2022-08-31 18:03:13 -0400

    Allow user to choose an excel file.

--- erg_scores.py
+++ erg_scores.py
@@ -146,7 +146,12 @@ if __name__ == '__main__':
     )
 
     if code == code_to_use:
-        wb = openpyxl.load_workbook("2022-07-17 Henley Erg Test.xlsx")
+        # wb = openpyxl.load_workbook("2022-07-17 Henley Erg Test.xlsx")
+        wb = None
+        while wb is None:
+            choice = st.file_uploader("Choose and Excel file")
+            st.write(choice)
+            wb = choice.read() if choice.read()[:-5] == ".xlsx" else None
         sheet = wb[wb.sheetnames[0]]
         scores_weight_yes = scores_to_dict(sheet, True)
         scores_weight_no = scores_to_dict(sheet, False)
