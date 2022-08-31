ad9b01aae5bb0dca79bf56c97ccf938589e16969
ranger522 <Daniel.Knorr@gmail.com>
2022-08-31 03:39:54 -0400

    Update erg_scores.py

--- erg_scores.py
+++ erg_scores.py
@@ -168,23 +168,27 @@ if __name__ == '__main__':
     # Sidebar
     # '''
     st.sidebar.header("Please select rowers (no more than 6): ")
-    rowers = st.sidebar.multiselect(
-        "Select the rowers:",
-        options=scores_pd["Name"]
+    code = st.sidebar.text_input(
+        "Enter code:"
     )
-    weight = st.sidebar.selectbox(
-        "Weight Adjust?",
-        options=["No", "Yes"]
-    )
-    scores_selection = scores_pd.query(
-        "Name == @rowers"
-    )
+    if code == "BRRC Henley":
+        rowers = st.sidebar.multiselect(
+            "Select the rowers:",
+            options=scores_pd["Name"]
+        )
+        weight = st.sidebar.selectbox(
+            "Weight Adjust?",
+            options=["No", "Yes"]
+        )
+        scores_selection = scores_pd.query(
+            "Name == @rowers"
+        )
 
-    wb = openpyxl.load_workbook("2022-07-17 Henley Erg Test.xlsx")
-    sheet = wb[wb.sheetnames[0]]
-    scores_weight_yes = scores_to_dict(sheet, True)
-    scores_weight_no = scores_to_dict(sheet, False)
+        wb = openpyxl.load_workbook("2022-07-17 Henley Erg Test.xlsx")
+        sheet = wb[wb.sheetnames[0]]
+        scores_weight_yes = scores_to_dict(sheet, True)
+        scores_weight_no = scores_to_dict(sheet, False)
 
-    weight_adjust = False if weight == "No" else True
-    scores = scores_weight_yes if weight_adjust else scores_weight_no
-    plot_splits(rowers, scores, dist=distance, weightAdjusted=weight_adjust)
+        weight_adjust = False if weight == "No" else True
+        scores = scores_weight_yes if weight_adjust else scores_weight_no
+        plot_splits(rowers, scores, dist=distance, weightAdjusted=weight_adjust)
