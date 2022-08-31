dbd827db43c636a34b57c6ad1e740d72e2a0a6ae
Dan Knorr <Daniel.Knorr@gmail.com>
2022-08-31 18:11:21 -0400

    Reverted back to user no longer choosing file.

--- erg_scores.py
+++ erg_scores.py
@@ -140,20 +140,13 @@ if __name__ == '__main__':
 
     st.set_page_config(page_title="Erg Scores",
                        layout="wide")
-    
+
     code = st.sidebar.text_input(
         "Enter code:"
     )
 
     if code == code_to_use:
-        # wb = openpyxl.load_workbook("2022-07-17 Henley Erg Test.xlsx")
-        wb = None
-        while wb is None:
-            choice = st.file_uploader("Choose an Excel file")
-            name = st.sidebar.text_input(
-                f"{Choice = }\n{choice.read() =}"
-            )
-            wb = choice.read() if choice.read()[:-5] == ".xlsx" else None
+        wb = openpyxl.load_workbook("2022-07-17 Henley Erg Test.xlsx")
         sheet = wb[wb.sheetnames[0]]
         scores_weight_yes = scores_to_dict(sheet, True)
         scores_weight_no = scores_to_dict(sheet, False)
