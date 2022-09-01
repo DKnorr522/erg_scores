9ec4bf21611d84e890da291740eb40ed71d7f3c2
Dan Knorr <Daniel.Knorr@gmail.com>
2022-09-01 01:30:13 -0400

    Messing around.

--- erg_scores.py
+++ erg_scores.py
@@ -25,7 +25,7 @@ def main():
     if code == code_to_use:
         pieces = os.listdir("pieces")
 
-        piece = st.sidebar.selectbox(
+        piece = st.sidebar.multiselect(
             "Choose a piece:",
             options=pieces
         )
@@ -34,7 +34,7 @@ def main():
 
         if piece:
             # wb = openpyxl.load_workbook("pieces/2022-07-17 Henley Erg Test.xlsx")
-            wb = openpyxl.load_workbook(f"pieces/{piece}")
+            wb = openpyxl.load_workbook(f"pieces/{piece[0]}")
             sheet = wb[wb.sheetnames[0]]
             # st.info(sheet)
             scores_weight_yes = scores_to_dict(sheet, True)
