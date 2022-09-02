abf0e488d7ade18305078bb2ca399dfef2c3a081
Dan Knorr <Daniel.Knorr@gmail.com>
2022-09-02 00:49:44 -0400

    Excel files now formatted "\d+m Tests.xlsx"

--- erg_scores.py
+++ erg_scores.py
@@ -23,63 +23,52 @@ def main():
 
         distances = []
 
-        pattern = r"\d+m.xlsx"
+        pattern = r"\d+m Tests.xlsx"
         dist_pattern = r"\d+"
 
         for file in files:
+            # Only cares about Excel files that are properly named
             if re.fullmatch(pattern, file):
                 dist = re.match(dist_pattern, file).group()
                 distances.append(dist)
 
         distance = st.sidebar.selectbox(
-            "Choose a distance",
+            "Choose a distance:",
             options=distances
         )
+        distance = int(distance)
 
-        # pieces = os.listdir("pieces")
-        #
-        # piece = st.sidebar.selectbox(
-        #     "Choose a piece:",
-        #     options=pieces
-        # )
+        wb = openpyxl.load_workbook(f"pieces/{distance}m Tests.xlsx")
+        piece = st.sidebar.selectbox(
+            "Choose a piece:",
+            options=wb.sheetnames
+        )
 
-        # if piece:
-        if distance:
-            wb = openpyxl.load_workbook(f"pieces/{distance}m.xlsx")
-            piece = st.sidebar.selectbox(
-                "Choose a piece:",
-                options=wb.sheetnames
-            )
-            distance = int(distance)
-            st.write(type(distance))
-            sheet = wb[piece]
+        sheet = wb[piece]
+        scores_weight_yes = scores_to_dict(sheet, True)
+        scores_weight_no = scores_to_dict(sheet, False)
 
-            # wb = openpyxl.load_workbook(f"pieces/{piece}")
-            # sheet = wb[wb.sheetnames[0]]
-            scores_weight_yes = scores_to_dict(sheet, True)
-            scores_weight_no = scores_to_dict(sheet, False)
+        st.sidebar.header("Please select rowers (no more than 6): ")
 
-            st.sidebar.header("Please select rowers (no more than 6): ")
+        # Allow multiple rowers to be selected
+        rowers = st.sidebar.multiselect(
+            "Select the rowers:",
+            options=scores_weight_yes.keys()
+        )
+        weight_adjust = st.sidebar.checkbox(
+            "Weight Adjust",
+            value=False
+        )
+        show_splits = st.sidebar.checkbox(
+            "Show Splits",
+            value=True
+        )
 
-            # Allow multiple rowers to be selected
-            rowers = st.sidebar.multiselect(
-                "Select the rowers:",
-                options=scores_weight_yes.keys()
-            )
-            weight_adjust = st.sidebar.checkbox(
-                "Weight Adjust",
-                value=False
-            )
-            show_splits = st.sidebar.checkbox(
-                "Show Splits",
-                value=True
-            )
-
-            scores = scores_weight_yes if weight_adjust else scores_weight_no  # select the relevant dictionary
-            # distance = wb[wb.sheetnames[1]].cell(row=1, column=1).value  # piece's distance is stored on sheet 2 cell A1
-            fig = plot_splits(rowers, scores, dist=distance, weightAdjusted=weight_adjust, showSplits=show_splits)
-            if fig:
-                st.pyplot(fig)
+        scores = scores_weight_yes if weight_adjust else scores_weight_no  # select the relevant dictionary
+        # distance = wb[wb.sheetnames[1]].cell(row=1, column=1).value  # piece's distance is stored on sheet 2 cell A1
+        fig = plot_splits(rowers, scores, dist=distance, weightAdjusted=weight_adjust, showSplits=show_splits)
+        if fig:
+            st.pyplot(fig)
 
 
 if __name__ == '__main__':
