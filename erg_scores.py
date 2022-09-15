9f95aa72c5f318fe81c700364dad0b98f12a3013
Dan Knorr <Daniel.Knorr@gmail.com>
2022-09-15 01:56:34 -0400

    Debugging

--- erg_scores.py
+++ erg_scores.py
@@ -77,7 +77,7 @@ def main():
         if fig:  # Without this a blank plot is shown until a name is selected
             st.pyplot(fig)
             # st.download_button("Save plot", fig, file_name=saveName)
-            st.download_button("Save plot")
+            st.download_button("Save plot", "data")
 
 
 if __name__ == '__main__':
